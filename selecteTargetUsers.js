431b697d74d1bde0df5315956e4d10140398f10a
htao <htao@thoughtworks.com>
2018-04-11 16:45:00 +0800

    [taohui] improve select user logic, filter out thoughtworkers and empty email users

--- selecteTargetUsers.js
+++ selecteTargetUsers.js
@@ -6,52 +6,86 @@ var sorted = items.slice(0, 1501);
 
 var selected = [];
 
-for(let i = 0; i < 5; i++){
+var count = 0;
+do {
   let random1 = getRandomInt(1, 30);
   console.log(random1);
-  selected.push(sorted[random1]);
-}
+  if(matchCondition(sorted[random1])) {
+    selected.push(sorted[random1]);
+    count++;
+  }
+} while(count < 5)
 
 console.log("--------------------------------");
 
-for(let i = 0; i < 20; i++){
+count = 0;
+do {
   let random1 = getRandomInt(30, 100);
   console.log(random1);
-  selected.push(sorted[random1]);
-}
+  if(matchCondition(sorted[random1])) {
+    selected.push(sorted[random1]);
+    count++;
+  }
+} while(count < 20)
+
 console.log("--------------------------------");
 
-for(let i = 0; i < 30; i++){
+count = 0;
+do {
   let random1 = getRandomInt(100, 400);
   console.log(random1);
-  selected.push(sorted[random1]);
-}
+  if(matchCondition(sorted[random1])) {
+    selected.push(sorted[random1]);
+    count++;
+  }
+} while(count < 30)
+
 console.log("--------------------------------");
 
-for(let i = 0; i < 30; i++){
+count = 0;
+do {
   let random1 = getRandomInt(400, 700);
   console.log(random1);
-  selected.push(sorted[random1]);
-}
+  if(matchCondition(sorted[random1])) {
+    selected.push(sorted[random1]);
+    count++;
+  }
+} while(count < 30)
+
 console.log("--------------------------------");
 
-for(let i = 0; i < 10; i++){
+count = 0;
+do {
   let random1 = getRandomInt(700, 1000);
   console.log(random1);
-  selected.push(sorted[random1]);
-}
+  if(matchCondition(sorted[random1])) {
+    selected.push(sorted[random1]);
+    count++;
+  }
+} while(count < 10)
+
 console.log("--------------------------------");
 
-for(let i = 0; i < 5; i++){
+count = 0;
+do {
   let random1 = getRandomInt(1000, 1500);
   console.log(random1);
-  selected.push(sorted[random1]);
-}
+  if(matchCondition(sorted[random1])) {
+    selected.push(sorted[random1]);
+    count++;
+  }
+} while(count < 5)
+
 console.log("--------------------------------");
 
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function matchCondition(item) {
+  return item.email && (item.company == null ||
+    item.company.toLowerCase().indexOf("thoughtworks") == -1);
 }
 
 fs.writeFile ("selected-items.json", JSON.stringify(selected), function(err) {
